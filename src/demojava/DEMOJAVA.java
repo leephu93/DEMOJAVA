0f7f74195cd460f6a5bb6b2b247dcc6ba7f9a87f
leephu93 <lee.phu93@gmail.com>
2018-11-15 12:53:24 +0700

    Xóa thư viện Scanner ở class DEMOJAVA

--- src/demojava/DEMOJAVA.java
+++ src/demojava/DEMOJAVA.java
@@ -5,9 +5,7 @@
  */
 package demojava;
 
-import java.util.Scanner;
-
-public class DEMOJAVA{
+public class DEMOJAVA {
 
     public static void main(String[] args) {
         System.out.println("INFO OF STUDENT: ");
