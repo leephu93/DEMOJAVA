2ca5583862ed4b5f5831ea9c671123dd03233960
leephu93 <lee.phu93@gmail.com>
2018-11-21 10:20:54 +0700

    Cập nhật 10:20 21/11/2018

--- src/demojava/DEMOJAVA.java
+++ src/demojava/DEMOJAVA.java
@@ -52,30 +52,30 @@ public class DEMOJAVA {
 //        System.out.println(sum(so1, so2));
 //        DEMOJAVA(9, 10);
 
-        ArrayList mang = new ArrayList();
-        float val = 0;
-        int count = 1;
-        System.out.println("HÃY NHẬP VÀO MỘT SỐ LẺ NGUYÊN DƯƠNG!");
-        do {
-            System.out.printf("\t\t NHẬP VÀO SỐ THỨ %d : ", count);
-            val = scanner.nextFloat();
-            if (val % 2 != 0) {
-                mang.add(val);
-            } else {
-                break;
-            }
-            count++;
-        } while (count <= 10);
-        System.out.println("KẾT QUẢ CỦA MẢNG HIỆN TẠI: " + mang.toString());
-        float total1 = 0, total2 = 0;
-        for (int i = 0; i < 5; i++) {
-            total1 = total1 + (float) mang.get(i);
-        }
-        System.out.println("TỔNG 5 PHẦN TỬ ĐẦU TIÊN CỦA MẢNG HIỆN TẠI: " + total1);
-        for (int i = mang.size() - 5; i < mang.size(); i++) {
-            total2 = total2 + (float) mang.get(i);
-        }
-        System.out.println("TỔNG 5 PHẦN TỬ CUỐI CÙNG CỦA MẢNG HIỆN TẠI: " + total2);
-        System.out.println("TỔNG CÁC PHẦN TỬ Ở VỊ TRÍ LẺ CỦA MẢNG HIỆN TẠI: " + (total1 + total2));
+//        ArrayList mang = new ArrayList();
+//        float val = 0;
+//        int count = 1;
+//        System.out.println("HÃY NHẬP VÀO MỘT SỐ LẺ NGUYÊN DƯƠNG!");
+//        do {
+//            System.out.printf("\t\t NHẬP VÀO SỐ THỨ %d : ", count);
+//            val = scanner.nextFloat();
+//            if (val % 2 != 0) {
+//                mang.add(val);
+//            } else {
+//                break;
+//            }
+//            count++;
+//        } while (count <= 10);
+//        System.out.println("KẾT QUẢ CỦA MẢNG HIỆN TẠI: " + mang.toString());
+//        float total1 = 0, total2 = 0;
+//        for (int i = 0; i < 5; i++) {
+//            total1 = total1 + (float) mang.get(i);
+//        }
+//        System.out.println("TỔNG 5 PHẦN TỬ ĐẦU TIÊN CỦA MẢNG HIỆN TẠI: " + total1);
+//        for (int i = mang.size() - 5; i < mang.size(); i++) {
+//            total2 = total2 + (float) mang.get(i);
+//        }
+//        System.out.println("TỔNG 5 PHẦN TỬ CUỐI CÙNG CỦA MẢNG HIỆN TẠI: " + total2);
+//        System.out.println("TỔNG CÁC PHẦN TỬ Ở VỊ TRÍ LẺ CỦA MẢNG HIỆN TẠI: " + (total1 + total2));
     }
 }
